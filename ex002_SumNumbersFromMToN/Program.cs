4013fc5861a71c93dfb60c9183c937bfe8097f88
MaximKisyushev <kisyushevmax@yandex.ru>
2022-09-10 14:31:27 +0500

    add ex 002 Sum numbers from M to N

---
+++ ex002_SumNumbersFromMToN/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число M: ");
+int startNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int endNumber = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int startNumber, int endNumber)
+{
+    if (startNumber == endNumber) return startNumber;
+
+    return (startNumber + SumNumbers(startNumber + 1, endNumber));
+}
+
+Console.WriteLine($"Сумма чисел от {startNumber} до {endNumber} --> {SumNumbers(startNumber, endNumber)}");
